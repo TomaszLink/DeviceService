11a0e42e25872d2bad00fecf49f2e332f7af252f
Tomasz Link <tomasz.link@core3.pl>
2026-07-03 21:10:04 +0200

    Revert "Added Location domain + kafka"
    This reverts commit 90421b6b7490839a9da299796d35802bab2033f8.

--- src/main/java/pl/tomaszlink/deviceservice/domain/location/kafka/DeviceLocationEventPublisher.java
+++
@@ -1,7 +0,0 @@
-package pl.tomaszlink.deviceservice.domain.location.kafka;
-
-import pl.tomaszlink.deviceservice.domain.location.models.DeviceLocationReceivedEvent;
-
-public interface DeviceLocationEventPublisher {
-    void publish(DeviceLocationReceivedEvent event);
-}
